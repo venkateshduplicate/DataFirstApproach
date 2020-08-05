dcf22f539ea2cd84fb02e1e337c8b758bafc5b79
venkatesh karamala <venkatesh.karamala@gmail.com>
2020-08-05 11:35:34 -0400

    Initial Commit

---
+++ WebApplication4/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        VenkyEnterpriseEntities db = new VenkyEnterpriseEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = db.Employees;
+            return View(employees);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if(!ModelState.IsValid)
+            {
+                throw new HttpException("Invalid Request");
+
+            }
+            db.Employees.Add(emp);
+            db.SaveChanges();
+
+           return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/EmployeeMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication4.Controllers;
+
+namespace WebApplication4.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+
+    }
+    public class EmployeeMetaData
+    {
+        [Required(ErrorMessage = "This is Required")]
+        public string EmpName { get; set; }
+    }
+}
